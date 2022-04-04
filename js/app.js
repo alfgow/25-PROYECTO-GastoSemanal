e2603b7dee684483d52eb8850bdcbba35e2e0b8c
alfgow <alfonso@villanuevagarcia.com>
2022-04-03 21:56:28 -0500

    ok

--- js/app.js
+++ js/app.js
@@ -14,39 +14,40 @@ function eventListeners() {
 class Presupuesto {
 	constructor(presupuesto) {
 		this.presupuesto = Number(presupuesto);
+		this.restante = Number(presupuesto);
+		this.gastos = [];
 	}
 }
+
+class UI {
+	insertarPresupuesto(cantidad) {
+		const { presupuesto, restante } = cantidad;
+		document.querySelector("#total").textContent = presupuesto;
+		document.querySelector("#restante").textContent = restante;
+	}
+}
+
+//! Instanciando
+let presupuesto;
+const ui = new UI();
 
 //! Funciones
 
 function preguntarPresupuesto() {
 	const presupuestoUsuario = prompt("Ingresa tu presupuesto semanal");
 
-	switch (presupuestoUsuario) {
-		case "":
-			alert("El presupuesto no puede estar vacio");
-			window.location.reload();
-			break;
-
-		case null:
-			alert("El presupuesto no puede estar vacio");
-			window.location.reload();
-			break;
-
-		default:
-			if (isNaN(presupuestoUsuario)) {
-				alert(
-					"El presupuesto no puedes estar en letras"
-				);
-				window.location.reload();
-			} else {
-				if (presupuestoUsuario <= 0) {
-					alert(
-						"El presupuesto no puede ser menor o igual a 0, ingresa un numero mayor a 0"
-					);
-					window.location.reload();
-				}
-			}
-			break;
+	if (
+		presupuestoUsuario == null ||
+		presupuestoUsuario == "" ||
+		isNaN(presupuestoUsuario) ||
+		presupuestoUsuario < 0
+	) {
+		alert("Debes ingresar un presupuesto valido");
+		preguntarPresupuesto();
 	}
+
+	//! Presupuesto valido
+	presupuesto = new Presupuesto(presupuestoUsuario);
+
+	ui.insertarPresupuesto(presupuesto);
 }
